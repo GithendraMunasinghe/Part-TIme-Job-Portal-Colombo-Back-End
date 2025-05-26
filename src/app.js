96316330ee28a8fe6dfe3b8b289ef1b370e9cb3d
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-05-26 21:44:34 +0530

    feat: add admin routes and services for roles, age groups, job titles, and regions; integrate helmet and morgan middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from 'cors'
+import morgan from "morgan";
+import helmet from "helmet";
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import resumeRouter from './routes/resumeRoutes.js'
+import adminRouter from './routes/adminRoutes.js'
+
 
 const app = express()
-
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json())
 app.use(cors())
 
@@ -16,5 +21,20 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 app.use('/api/resume', resumeRouter)
+app.use('/api/admin', adminRouter)
+
+app.use((req, res) => {
+    res.status(404).json({ error: "Route Not Found" });
+});
+
+app.use((err, req, res, next) => {
+    console.error("Error:", err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+process.on("SIGINT", () => {
+    console.log("Server shutting down...");
+    process.exit();
+});
 
 export default app;
