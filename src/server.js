a753fd7840ce2345777b58070da6489fdf2bb297
Githendra Munasinghe <geethmunasinghe335@gmail.com>
2025-07-19 13:17:07 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import { PORT } from "./config/env.js";
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
+app.listen(5000, () => {
+    console.log(`server is running on port ${5000}`)
 })
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
-import { JwT_SECRET } from '../config/env.js'
+//import { JwT_SECRET } from '../config/env.js'
+const JwT_SECRET= "1a466a6e3eeaaa0807167119e9a3092585d63b839da47151c2ac9aa592a16e26b873055ab7b989a31eab9066d9b9b4584319c4586adb5fcd3984e53c453bc7db"
 
 export const generatedToken = (payload) => {
     if (!JwT_SECRET) {
